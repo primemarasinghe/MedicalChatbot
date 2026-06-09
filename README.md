9f676b443e0c512fa08ad9aaa38faf7ea37ae4cf
Primesh Marasingha <primeshmarasinghe@gmail.com>
2026-06-09 15:16:08 +0530

    Added requirement libraries

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# MedicalChatbot
+#check for env
+conda env list 
+#MedicalChatbot env create for python
+conda create -n medibotenv python=3.11 -y
+#activate the env
+conda activate medibotenv
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+setup(name="MedicalChatbot",
+      version="0.1",
+      author="Primesh",
+      description="A medical chatbot " \
+      "built using Langchain and Pinecone",
+      author_email="primeshmarasingha@gmail.com",
+      packages=find_packages(),
+      install_requires=[]
+      )
